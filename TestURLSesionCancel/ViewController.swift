8ad8231be0f1888ce7615725bf87b5870c2fcbed
Tsungyu Yu <ytyubox@gmail.com>
2020-03-19 16:24:41 +0800

    Initial Commit

---
+++ TestURLSesionCancel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestURLSesionCancel
+//
+//  Created by 游宗諭 on 2020/3/19.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
